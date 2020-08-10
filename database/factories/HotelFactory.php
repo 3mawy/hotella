503e8e37aadc7e59c6ea06b77e9d8ebd4c4e610e
3mawy <m.3mawyy@gmail.com>
2020-08-10 10:06:15 +0200

    m

--- database/factories/HotelFactory.php
+++ database/factories/HotelFactory.php
@@ -8,19 +8,7 @@ use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(Hotel::class, function (Faker $faker) {
-    $images =['https://exp.cdn-hotels.com/hotels/46000000/45490000/45484100/45484010/37eb65cb_l.jpg',
-        'https://exp.cdn-hotels.com/hotels/6000000/5360000/5359900/5359834/3dd8462e_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/6000000/5360000/5359900/5359834/d30ffa7d_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/6000000/5360000/5359900/5359834/ffc6c36f_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/28000000/27920000/27918500/27918471/9428cc2b_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/28000000/27920000/27918500/27918471/74635c3b_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/28000000/27920000/27918500/27918471/9428cc2b_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/28000000/27920000/27918500/27918471/046fd6a5_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/2000000/1400000/1397500/1397438/da7c4c85_y.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/2000000/1400000/1397500/1397438/da7c4c85_y.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/2000000/1400000/1397500/1397438/a5833765_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-        'https://exp.cdn-hotels.com/hotels/2000000/1400000/1397500/1397438/5cbe299d_z.jpg?impolicy=fcrop&w=773&h=530&q=high',
-    ];
+    $images =['https://imgur.com/BSezryi','https://imgur.com/KqIym5m'];
     $overviews = ['Elite Residence & Aqua Park boasts a restaurant. The hotel has an outdoor pool, and indoor heated swimming pool and a private beach area, and guests can enjoy a meal at the restaurant or a drink at the bar. Free private parking is available on site.
 
 Each room at this hotel is air conditioned and has a flat-screen TV with satellite channels. Certain units have a seating area for your convenience. Some rooms feature views of the sea or pool. Each room is fitted with a private bathroom. For your comfort, you will find free toiletries and a hair dryer.
